e2e32d1cfb041b7f9b5314e2e618ddb2338804ba
sdolas <sdolas0909@gmail.com>
2023-03-19 00:01:46 +0100

    rename

--- get_articles.py
+++ get_articles.py
@@ -7,11 +7,6 @@ import time
 from selenium import webdriver as wd
 import sqlite3
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 
 # Verbindung zur Datenbank herstellen
 # Tabelle erstellen
